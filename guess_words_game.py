e249fe1b15de6e7a6a40fd8ed607a90f3d6c7d31
Elnaz Dehkharghani <elnaz.dehkharghani2gmail.com>
2021-07-10 16:29:39 +0200

    Guess words Game

---
+++ guess_words_game.py
@@ -0,0 +1,15 @@
+list_of_names = ['Elnaz', 'Sanaz', 'Farnaz',
+                 'Hamid', 'Fariba', 'Hassan', 'Elyas', 'Setia']
+correct_guess = 'setia'
+print(list_of_names)
+your_geuss = input('Please enter your guess word:\n')
+for i in range(5):
+    if your_geuss == correct_guess:
+        print('You Won!')
+        break
+    elif i == 4:
+        print('You are loser!')
+    else:
+        print(f'No, It is not correct ! you can try {4-i} times more')
+        your_geuss = input('Try again:\n')
+print(f'Correct word was {correct_guess}!')
